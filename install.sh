2f0188e26ccd32af05747db459007f462514fb6a
Leonard Chin <l.g.chin@gmail.com>
2010-03-07 00:27:31 +0900

    Fix installer for case where links already exist
    Order of conditionals was incorrect. Changed to check symlink first.

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@ for f in $FILES
 do
   if [ $f != "$BASEDIR/install.sh" -a $f != "$BASEDIR/install" ]; then
     target="$HOME/.`basename $f`"
-    if [ -f $target -o -d $target ]; then
+    if [ -L $target ]; then
+      echo "$target already installed"
+    elif [ -f $target -o -d $target ]; then
       mv $target "${target}.old"
       echo "Replacing $target (backup to ${target}.old)"
       ln -s $f $target
-    elif [ -L $target ]; then
-      echo "$target already installed"
     else
       echo "Installing $target"
       ln -s $f $target
