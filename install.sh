2020dbd31d80b8d43f4c8ec0f530b21e574ebab5
Leonard Chin <l.g.chin@gmail.com>
2020-04-01 09:25:47 +0900

    Remove stale lines from installation script

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ __link_with_backup() {
 
 __link_dotfile() {
 	local f=$1
-	if [ $f != $BASEDIR/install.sh -a $f != $BASEDIR/update_servers.sh -a $f != "$BASEDIR/install" ]; then
+	if [ $f != $BASEDIR/install.sh ]; then
 		local target="$HOME/.`basename $f`"
 		__link_with_backup $f $target
 	fi
@@ -34,23 +34,3 @@ done
 mkdir -p $HOME/bin
 mkdir -p $HOME/opt/bin
 mkdir -p $HOME/.ssh
-if [ ! -x "$HOME/bin/ack" ]; then
-  echo "Installing ack (standalone)"
-  curl http://betterthangrep.com/ack-standalone > $HOME/bin/ack
-  chmod 0755 $HOME/bin/ack
-fi
-
-if [ ! -x "$HOME/bin/hub" ]; then
-  echo "Installing hub (standalone)"
-  curl http://defunkt.io/hub/standalone -sLo $HOME/bin/hub
-  chmod 0755 $HOME/bin/hub
-fi
-
-if [ -a "$HOME/.ssh_config" ]; then
-	__link_with_backup $HOME/.ssh_config $HOME/.ssh/config
-fi
-
-# Compile tmux MacOSX pasteboard
-if [ -a "$HOME/.modules/tmux-pasteboard/Makefile" ]; then
-	(cd ~/.modules/tmux-pasteboard; make reattach-to-user-namespace && cp reattach-to-user-namespace ~/bin)
-fi
