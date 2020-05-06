3a02d6df346970411e9651e032263c90a80da75b
Leonard Chin <l.g.chin@gmail.com>
2020-05-06 16:22:06 +0900

    Local config for fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
+set -g GIT_EDITOR 'nvim'
+set -g EDITOR 'nvim'
+
 # Enable rbenv
 status --is-interactive; and source (rbenv init -|psub)
 
@@ -7,4 +10,8 @@ starship init fish | source
 # jump: https://github.com/gsamokovarov/jump
 if which -s jump
 	status --is-interactive; and source (jump shell fish | psub)
+end
+
+if test -e ~/.config/fish/localconfig.fish
+  source ~/.config/fish/localconfig.fish
 end
